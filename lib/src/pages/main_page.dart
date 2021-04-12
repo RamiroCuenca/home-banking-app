dda2c89dd2466abb5dcb1c67a768bed03826446b
Ramiro Cuenca <ramicuenca00@gmail.com>
2021-04-12 18:06:39 -0300

    Created bottomNavigationBar at main_page.dart

--- lib/src/pages/main_page.dart
+++ lib/src/pages/main_page.dart
@@ -6,8 +6,21 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text('Main page'),
-    ));
+      appBar: AppBar(
+        title: Text('Main page'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.credit_card), label: 'Cards'),
+          BottomNavigationBarItem(icon: Icon(Icons.receipt), label: 'Payments'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.swap_horiz), label: 'Transfers'),
+          BottomNavigationBarItem(icon: Icon(Icons.menu), label: 'More'),
+        ],
+      ),
+    );
   }
 }
